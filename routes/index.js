e6caabb4d0cdcf797c2fc8028eaf4b591ac410a3
Flavio Brito <125912752+Makeart1@users.noreply.github.com>
2023-04-25 20:44:00 -0300

    b-08 (#7)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,43 @@
 /* var express = require('express'); */
 import express from 'express'
+import userRouter from './users.js'
+import authorRouter from './authors.js'
+import categoryRouter from './categories.js'
+import companyRouter from './companies.js'
+import mangaRouter from './mangas.js'
+import chapterRouter from './chapters.js'
+
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'MINGA API',
+    subtitle:'endpoint of minga' });
 });
 
+router.use('/auth',userRouter)
+router.use('/authors',authorRouter)
+router.use('/cateogires',categoryRouter)
+router.use('/companies',companyRouter)
+router.use('/mangas',mangaRouter)
+router.use('/chapters',chapterRouter)
 export default router;
+
+/* let endpoint = '/users'
+function funcionDelEndpoint(req, res, next) {
+    res.send('respond with a resource');
+}    */
+
+/* router.get('/products',(req,res,next)=>{
+  return res.status(200).send('se encontraron productos para el endpoint')
+})
+
+router.get ('/carts',(req,res,next)=>res.status(200).json({
+  carts:[],
+  success:true
+})) */
+
+/* router.get(endpoint,funcionDelEndpoint) */
+
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 /* var express = require('express'); */
-const router = express.Router();
 import express from 'express'
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/admins',(rq,res,next)=>res.status(200).json({
+  success:true,
+  admins:[]
+}))
 export default router;
